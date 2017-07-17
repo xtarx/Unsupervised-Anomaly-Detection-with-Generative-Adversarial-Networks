ba252e923980981fdf35c5bad31d616bc8a79639
XtarX <xtarx0@gmail.com>
2017-07-17 20:40:50 +0200

    inception score

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 import numpy as np
-# import inception_score
+import inception_score
 
 from model import DCGAN
 from utils import *
@@ -26,7 +26,7 @@ flags.DEFINE_string("dataset", "mias", "The name of dataset [celebA, mnist, lsun
 flags.DEFINE_string("input_fname_pattern", "*.jpg", "Glob pattern of filename of input images [*]")
 flags.DEFINE_string("checkpoint_dir", "checkpoint", "Directory name to save the checkpoints [checkpoint]")
 flags.DEFINE_string("sample_dir", "samples", "Directory name to save the image samples [samples]")
-flags.DEFINE_boolean("train", True, "True for training, False for testing [False]")
+flags.DEFINE_boolean("train", False, "True for training, False for testing [False]")
 flags.DEFINE_boolean("crop", False, "True for training, False for testing [False]")
 flags.DEFINE_boolean("visualize", False, "True for visualizing, False for nothing [False]")
 FLAGS = flags.FLAGS
@@ -88,51 +88,51 @@ def main(_):
 
         # calculate inception score
 
-        #
-        #     test_list = []
-        #     gen_list = []
-        #
-        #     arr = os.listdir(os.getcwd() + "/data/test/")
-        #     for test_image in arr:
-        #         query_image_path = os.getcwd() + "/data/test/" + test_image
-        #         c_dim = imread(query_image_path).shape[-1]
-        #         grayscale = (c_dim == 1)
-        #         q_img = get_image(query_image_path,
-        #                           input_height=FLAGS.input_height,
-        #                           input_width=FLAGS.input_width,
-        #                           resize_height=FLAGS.output_height,
-        #                           resize_width=FLAGS.output_width,
-        #                           crop=FLAGS.crop,
-        #                           grayscale=grayscale);
-        #         # q_img *= 255.0
-        #         test_list.append(q_img);
-        #
-        #     arr = os.listdir(os.getcwd() + "/data/testGANs/")
-        #     arr = arr[1:]
-        #     print(arr)
-        #     for test_image in arr:
-        #         query_image_path = os.getcwd() + "/data/testGANs/" + test_image
-        #         c_dim = imread(query_image_path).shape[-1]
-        #         grayscale = (c_dim == 1)
-        #         q_img = get_image(query_image_path,
-        #                           input_height=64,
-        #                           input_width=64,
-        #                           resize_height=FLAGS.output_height,
-        #                           resize_width=FLAGS.output_width,
-        #                           crop=False,
-        #                           grayscale=grayscale);
-        #         # q_img *= 255.0
-        #         gen_list.append(q_img);
-        #
-        # # print((test_list[0]))
-        #
-        # real = inception_score.get_inception_score(test_list)
-        # print('real inception: {}'.format(real))
-        # print("------------")
-        # fake = inception_score.get_inception_score(gen_list)
-        # print('fake inception: {}'.format(fake))
-        #
-        #
+
+        test_list = []
+        gen_list = []
+
+        arr = os.listdir(os.getcwd() + "/data/test/")
+        for test_image in arr:
+            query_image_path = os.getcwd() + "/data/test/" + test_image
+            c_dim = imread(query_image_path).shape[-1]
+            grayscale = (c_dim == 1)
+            q_img = get_image(query_image_path,
+                              input_height=FLAGS.input_height,
+                              input_width=FLAGS.input_width,
+                              resize_height=FLAGS.output_height,
+                              resize_width=FLAGS.output_width,
+                              crop=FLAGS.crop,
+                              grayscale=grayscale);
+            # q_img *= 255.0
+            test_list.append(q_img);
+
+        arr = os.listdir(os.getcwd() + "/data/testGANs/")
+        arr = arr[1:]
+        print(arr)
+        for test_image in arr:
+            query_image_path = os.getcwd() + "/data/testGANs/" + test_image
+            c_dim = imread(query_image_path).shape[-1]
+            grayscale = (c_dim == 1)
+            q_img = get_image(query_image_path,
+                              input_height=64,
+                              input_width=64,
+                              resize_height=FLAGS.output_height,
+                              resize_width=FLAGS.output_width,
+                              crop=False,
+                              grayscale=grayscale);
+            # q_img *= 255.0
+            gen_list.append(q_img);
+
+    # print((test_list[0]))
+
+    real = inception_score.get_inception_score(test_list)
+    print('real inception: {}'.format(real))
+    print("------------")
+    fake = inception_score.get_inception_score(gen_list)
+    print('fake inception: {}'.format(fake))
+
+
 
         # print(query_image);
         # generate_latent_for_query(sess, dcgan, query_image, FLAGS=FLAGS, OPTION=OPTION)
